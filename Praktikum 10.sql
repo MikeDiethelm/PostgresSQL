7794ce65c4478b1d06e02383bd8a7e078298cc83
Mike Diethelm <mike_diethelm93@bluewin.ch>
2022-12-31 15:36:48 +0100

    added p10 vorlage

---
+++ Praktikum 10.sql
@@ -0,0 +1,44 @@
+-- Praktikum 10
+-- 1) Wie hoch ist der durchschnittliche Lagerbestand aller Biere (der Wert soll den Namen Durchschnittslagerbestand erhalten)?
+
+
+-- 2) Welche Besucher (Name,Vorname, Strasse) wohnen an einer Strasse, deren Bezeichnung das Wort ‚bach‘ enthält?
+-- Untersuchen Sie auch die Resultate, die Sie mit ,Bach‘, ‚BACH‘ etc. erhal- ten. Sehen Sie hier allenfalls ein Problem?
+
+
+-- 3) An welchen Strassen gibt es mindestens drei Restaurants (GROUP BY verwenden)?
+
+
+-- 4) Bilden Sie das Kreuzprodukt der Tabellen Restaurant und Besucher (mit und ohne CROSS JOIN).
+-- Wieviele Tupel enthält das Resultat? Warum? Wozu könnte eine solche Abfrage nützlich sein?
+
+
+-- 5) Gesucht ist eine Liste von Besuchern mit Name, Vorname und der Anzahl Restaurantbesuche pro Woche (= Frequenz).
+-- Falls ein Besucher nie Gast ist, soll er auf der Liste mit einer Anzahl Besuche von 0 erscheinen.
+-- Verwenden Sie dazu einen OUTER JOIN. Optional: Lösung ohne JOIN.
+
+
+-- 6) Gesucht ist eine Liste der Hersteller von Biersorten zusammen mit der Anzahl Biersorten, die
+-- sie produzieren und der Anzahl verschiedener dabei verwendeter Grundstoffe.
+
+
+-- 7) Welche Biersorten sind von allen mit derselben Note bewertet worden (Hinweis: das bedeutet,
+-- dass kleinste Note = grösste Note)?
+
+
+-- 8) Gesucht ist eine Liste von Restaurants mit ihrem Namen, ihrem Suppenpreis, sowie dem durch-
+-- schnittlichen Suppenpreis aller Restaurants derselben Strasse.
+
+
+-- 9) Gesucht ist eine Liste von Restaurants und Biersorten, von denen sie am meisten an Lager
+-- haben.
+
+
+-- 10) Gesucht sind die Strassen, an denen es mehr Restaurants gibt als Besucher.
+
+
+-- 11) Gesucht ist eine Liste der Strassen von Besuchern, deren Vornamen den Buchstaben “p“ ent-
+-- hält und die in Restaurants verkehren mit einem Suppenpreis zwischen 3 und 5
+
+
+-- 12) Gesucht ist die Anzahl verschiedener Besucher des Restaurant Löwen.
