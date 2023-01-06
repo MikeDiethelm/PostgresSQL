752f904788e971afa1c5c0845032aec92d4e536d
Mike Diethelm <mike_diethelm93@bluewin.ch>
2023-01-06 02:05:47 +0100

    prepared p12 for solving

---
+++ Praktikum 12.sql
@@ -0,0 +1,34 @@
+-- 1) Finden Sie die Teilenummern von allen Teilen, welche von einem Lieferanten aus derselben
+-- Stadt geliefert werden, wie das Projekt, für welches das Teil geliefert wird.
+
+
+-- 2) Finden Sie die Gesamtmenge von Teil ‚T1‘, die Sulzer (für irgendein Projekt) liefert.
+
+
+-- 3) Finden Sie die Teilenummern aller Teile, welche an mindestens ein Projekt mit durchschnittli-
+-- cher Menge grösser als 350 geliefert werden.
+
+
+-- 4) Finden Sie die Projektnamen aller Projekte, die durch Sulzer beliefert werden.
+
+
+-- 5) Finden Sie die Farben aller Teile, die durch Sulzer geliefert werden.
+
+
+-- 6) Finden Sie die Teilenummern aller Teile, welche an irgendein Projekt in Winterthur geliefert
+-- werden.
+
+
+-- 7) Finden Sie die Projektnummern aller Projekte, welche mindestens ein Teil geliefert bekom-
+-- men, das auch von Sulzer (irgendwohin) geliefert wird.
+
+
+-- 8) Finden Sie alle Lieferanten/Teile-Paare, bei denen der Lieferant das Teil nicht liefert.
+
+
+-- 9) Finden Sie die Lieferantennummern aller Lieferanten, welche das Teil mit der Nummer ‚T1‘ in
+-- einer Menge liefern, die grösser ist als die durchschnittliche Menge von Teilen ‚T1‘, welche an
+-- dasselbe Projekt geliefert werden.
+
+
+-- 10) Finden Sie alle Städte, in denen sich mindestens ein Lieferant, ein Teil oder ein Projekt befindet
